4dd5d09e5acf5dc77c92fdf8ae61c56f36f3a708
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-01 16:47:17 -0600

    Added router and switched from previous code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home'; // import our pages 
+import Contact from './pages/Saved';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Home' component={Home} />
+          <Route exact path='/Saved' component={Saved} />
+        </Switch>
+      </Router>
     </div>
   );
 }
