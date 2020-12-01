4d40ff62381ddf20e0d05faf60144b646fd23740
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-01 14:12:02 -0600

    Changed the database

--- models/book.js
+++ models/book.js
@@ -4,10 +4,15 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  description: String,
+  img:
+  {
+    data: Buffer,
+    contentType: String
+  },
+  link: { type: String, required: true }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const googlebooks = mongoose.model("googlebooks", googlebooks);
 
-module.exports = Book;
+module.exports = googlebooks;
