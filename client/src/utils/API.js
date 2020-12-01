132bc204cf1e68bc37879e6f0b809c33d1034c0f
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-01 16:42:48 -0600

    Removed boiler plate css

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,17 @@ import axios from "axios";
 export default {
     search: function(query) {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    },
+
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
     }
 };
