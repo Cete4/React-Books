b09345d0bc568d90211dd81016aa488ebd7448d3
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-01 21:44:44 -0600

    Getting a wierd error and dont know whats wrong

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home'; // import our pages 
-import Contact from './pages/Saved';
+import Saved from './pages/Saved';
 
 
 function App() {
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { Component } from "react";
 import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
@@ -9,7 +9,7 @@ import { Input, TextArea, FormBtn } from "../components/Form";
 import BookCard from "../components/BookCard";
 
 
-function Home() {
+class Home extends Component {
     state = {
         value: "",
         books: []
@@ -24,79 +24,63 @@ function Home() {
                         search: ""
                     },
                     console.log(res.data.items)
-                )
-            )
-            .catch(err => console.log(err));
-    };
-
-    // Loads all books and sets them to books
-    function loadBooks() {
-        API.getBooks()
-            .then(res =>
-                setBooks(res.data)
-            )
+                ))
             .catch(err => console.log(err));
     };
 
     // Handles updating component state when the user types into the input field
     function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormObject({ ...formObject, [name]: value })
-    };
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+        [name]: value
+    });
+};
 
-    // When the form is submitted, use the API.saveBook method to save the book data
-    // Then reload books from the database
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        if (formObject.title && formObject.author) {
-            API.saveBook({
-                title: formObject.title,
-                author: formObject.author,
-                synopsis: formObject.synopsis
-            })
-                .then(res => loadBooks())
-                .catch(err => console.log(err));
-        }
-    };
+// When the form is submitted, use the API.saveBook method to save the book data
+// Then reload books from the database
+function handleFormSubmit(event) {
+    event.preventDefault();
+    this.search(this.state.search);
+};
 
-    return (
-        <Container fluid>
-            <Row>
-                <Col size="md-12">
-                    <Jumbotron>
-                        <h1>Books</h1>
-                    </Jumbotron>
+return (
+    <Container fluid>
+        <Row>
+            <Col size="md-12">
+                <Jumbotron>
+                    <h1>Books</h1>
+                </Jumbotron>
 
 
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    {this.state.map.books.map(book => (
-                        <BookCard
-                            key={book.id}
-                            src={book.items.imageLinks.smallThumbnail}
-                            title={book.volumeInfo.authors[0]}
-                            date={book.volumeInfo.publishedDate}
-                            description={book.volumeInfo.description}
-                            link={book.volumeInfo.infoLink}
-                            link={book.volumeInfo.infoLink}
-                            handleSaveBook={() => this.handleSaveBook({
-                                title: book.volumeInfo.title,
-                                src: book.volumeInfo.imageLinks,
-                                author: book.volumeInfo.authors,
-                                date: book.volumeInfo.publishedDate,
-                                description: book.volumeInfo.description,
-                                link: book.volumeInfo.infoLink
-                            })}
-                        />
-
-                    ))}
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                {this.state.map.books.map(book => (
+                    <BookCard
+                        key={book.id}
+                        src={book.items.imageLinks.smallThumbnail}
+                        title={book.volumeInfo.authors[0]}
+                        date={book.volumeInfo.publishedDate}
+                        description={book.volumeInfo.description}
+                        link={book.volumeInfo.infoLink}
+                        link={book.volumeInfo.infoLink}
+                        handleSaveBook={() => this.handleSaveBook({
+                            title: book.volumeInfo.title,
+                            src: book.volumeInfo.imageLinks,
+                            author: book.volumeInfo.authors,
+                            date: book.volumeInfo.publishedDate,
+                            description: book.volumeInfo.description,
+                            link: book.volumeInfo.infoLink
+                        })}
+                    />
+                ))}
+            </Col>
+        </Row>
+    </Container>
+);
+                    }
 
 
 export default Home;
