e7ac37217a02d9363adb4187deaaf535a1934de2
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-01 22:10:54 -0600

    Changed for cohesion

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: String,
+  description: { type: String, required: true },
   img: { type: String, required: true },
   link: { type: String, required: true }
 });
