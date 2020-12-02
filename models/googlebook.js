8c44ee553985bc1b71cb5a0ee86088e1b4342410
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-12-01 22:12:49 -0600

    More changes that I missed

--- models/googlebook.js
+++ models/googlebook.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const googlebooks = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   description: { type: String, required: true },
